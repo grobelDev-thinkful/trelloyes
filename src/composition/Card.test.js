48869b96292c2a16083cf6f7235b01b7be60f959
grobelDev <grobeldev@gmail.com>
2019-11-05 13:27:59 -0800

    put in smoke test for Card.js

---
+++ src/composition/Card.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+import Card from './Card';
+
+
+it('renders without crashing', () => {
+
+    const div = document.createElement('div');
+    ReactDOM.render(<Card />, div);
+    ReactDOM.unmountComponentAtNode(div);
+
+});
+  
+
+// import './Card.css';
+
+
